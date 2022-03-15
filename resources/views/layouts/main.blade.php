f881acaa832cfb513ffebdecd67dea2ea5303d55
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-15 16:32:18 +0100

    added scss generics, vars and main.blade.php

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dc Comics | Home</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+
+<body>
+    <section class="container">
+        <figure>
+            <img src="" alt="" />
+        </figure>
+        <nav>
+            <ul>
+                <li></li>
+            </ul>
+        </nav>
+    </section>
+    <main>
+        <div id="content">
+            <span id="current">CURRENT SERIES</span>
+            <section id="jumbo"></section>
+            <section class="container">
+                <section>
+                    <ul id="cards">
+                        <li class="card-game">
+                            <img src="product.thumb" alt="" />
+                            <p></p>
+                        </li>
+                    </ul>
+                    <span id="load-button"><button><strong>LOAD MORE</strong></button></span>
+                </section>
+            </section>
+        </div>
+        <div id="main-footer-container">
+            <section class="container" id="figures">
+                <figure>
+                    <img src="" alt="" />
+
+                </figure>
+            </section>
+        </div>
+    </main>
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
+</html>
