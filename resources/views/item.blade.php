1085b6efde799c2cc3c9359e1335682cd4d06a8c
GiulioSpugnini <giuliospugnini@yahoo.com>
2022-03-17 01:52:12 +0100

    adjusted item layout

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -15,12 +15,12 @@
             <img src="{{ asset('images/advs.jpg') }}" alt="adv">
         </div>
         <div class="talent-specs">
-            <div>
+            <div class="w-50">
                 <h5>Talent</h5>
                 <h6>Art by: $item['artists'] </h6>
                 <h6>Written by: $item['writers'] </h6>
             </div>
-            <div>
+            <div class="w-50">
                 <h5>Specs</h5>
                 <h6>Series: $item['series'] </h6>
                 <h6>U.S Price: $item['price'] </h6>
